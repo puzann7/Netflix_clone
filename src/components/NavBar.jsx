d240ef0f0c45f58c1a958e4c6c45260d1f200117
puzann7 <poojanbhatt78@gmail.com>
2025-04-03 22:08:16 +0545

    create home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      <ul className="text-white">
+    <div className="">
+      <ul className="text-white flex flex-wrap justify-between p-4 items-center ">
         <NavLink to="/home">
-          <img src="../assets/netflix_logo.png" alt="" />
+          <img className="w-10 " src="/assets/image-removebg-preview.png" alt="not valid" />
         </NavLink>
-        <NavLink to="/home">Home</NavLink>
         <NavLink to="/movies">Movies</NavLink>
         <NavLink to="/tvshows">TV Shows</NavLink>
+        <NavLink to="/tvshows">My List </NavLink>
       </ul>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
+import { fetchTrending } from "../data/fetchReq";
+import { IMG_URL } from "../data/apiUrl";
 
 function Home() {
-  return (
-    <div className="bg-black h-screen w-screen">
-        <NavBar />
-    </div>
-  )
+  const [mostTrendingMovie, setMostTrendingMovie] = useState({});
+  const homePosterImage = `${IMG_URL}${mostTrendingMovie.poster_path}`;
+  const getMostTrendingMovie = async () => {
+    const trendings = await fetchTrending();
+    setMostTrendingMovie(trendings.data.results[0]);
+  };
+  getMostTrendingMovie();
 
+  return (
+    <div className="container bg-black h-screen w-screen  ">
+    <div
+      style={{ backgroundImage: `url(${homePosterImage})` }}
+      className={`  h-[60%] w-full  bg-center bg-cover`}
+    >
+      <NavBar />
+    </div>
+      <h1 className="text-white text-center ">#1 Trending Today</h1>
+      <div className="flex text-white justify-around p-4 ">
+        <div>My List</div>
+        <div>Play</div>
+        <div>
+            <img src="../../public/assets/icons/info.png" alt="" />
+        Info</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
