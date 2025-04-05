e355c7915aa9f1150585061e0a8e1f28591dad3c
puzann7 <poojanbhatt78@gmail.com>
2025-04-05 10:08:09 +0545

    add movie preview icon components

---
+++ src/components/Previews.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MoviePreview from '../Small components/MoviePreview'
+
+function Previews() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Previews
