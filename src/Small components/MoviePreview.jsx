2541063c22d43286a39443bc0b1c5868f7c3d678
puzann7 <poojanbhatt78@gmail.com>
2025-04-05 11:23:54 +0545

    work on preview section

--- src/Small components/MoviePreview.jsx
+++ src/Small components/MoviePreview.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MoviePreview({ image}) {
   return (
     <div className=''>
-        <img  className="w-[102px] h-[102px]   rounded-full object-cover object-top"src="https://m.media-amazon.com/images/I/71V4mbT4n9L._AC_UF894,1000_QL80_.jpg" alt="" />
+        <img  className="w-[102px] h-[102px]   rounded-full object-cover object-top" src={image} alt="" />
     </div>
   )
 }
--- src/components/Previews.jsx
+++ src/components/Previews.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MoviePreview from '../Small components/MoviePreview'
+import useMovieData from '../contexts/Movie Category/movieContext'
+import { IMG_URL } from '../data/apiUrl';
 
 function Previews() {
+    const [trendingPoster, setTrendingPoster] = useState();
+    const {trendings} = useMovieData();
+    useEffect(() => {
+        trendings.length>0 && setTrendingPoster(trendings.map((movie)=>{
+           return movie.poster_path;
+        }) )
+    }, [trendings])
+    console.log(trendingPoster);
+
+
+
   return (
-    <div>
-        
+    <div className='bg-red-500 h-[18vh] '>
+        <h1 className='text-white'>Previews</h1>
+    <div className=''>
+        {trendingPoster && trendingPoster.map((poster)=>(
+            <MoviePreview image={`${IMG_URL}${poster}`} />
+        ))}
+    </div>
     </div>
   )
 }
