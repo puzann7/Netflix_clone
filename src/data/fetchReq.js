2344c5bccea477a9f820f160a122539e3b82326c
puzann7 <poojanbhatt78@gmail.com>
2025-04-05 10:59:06 +0545

    make the fetch requests for movies for optimized

--- src/data/fetchReq.js
+++ src/data/fetchReq.js
@@ -3,7 +3,7 @@ import requests from "./apiUrl";
 const fetchTrending =  async ()=>{
     try {
         const response = await axios.get(requests.fetchTrending);
-        return response;
+        return response.data.results;
     } catch (error) {
         console.log(error);
     }
