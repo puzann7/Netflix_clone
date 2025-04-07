97f57ca01dc9f7e9f39f50bcb7aa05d064339502
puzann7 <poojanbhatt78@gmail.com>
2025-04-07 15:16:08 +0545

    make responsive nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="absolute top-0  z-10 w-full ">
-      <ul className="text-white md:text-2xl flex flex-wrap justify-between p-4 md:py-6 px-8 md:px-12 items-center ">
+    <div className="absolute top-0  z-10 w-full lg:flex py-4  px-8 md:px-6  justify-between  ">
+      <ul className="text-white md:text-2xl lg:text-sm flex flex-wrap justify-between lg:justify-start lg:gap-8 items-center  ">
         <NavLink to="/home">
         <picture>
             <source media="(min-width: 768px)" srcSet="../../public/assets/netflix_logo.png" />
           <img
-            className="w-10 lg:w-48 "
+            className="w-10 lg:w-36 "
             src="/assets/image-removebg-preview.png"
             alt="not valid"
           />
@@ -19,6 +19,10 @@ const NavBar = () => {
         <NavLink to="/tvshows">TV Shows</NavLink>
         <NavLink to="/tvshows">My List </NavLink>
       </ul>
+      <div className="hidden lg:flex lg:items-center lg:gap-8 ">
+        <img className="w-5 h-5 cursor-pointer" src="assets/icons/search.png" />
+        <img className="w-5 h-5 cursor-pointer" src="assets/icons/bell.png" alt="" />
+      </div>
     </div>
   );
 };
