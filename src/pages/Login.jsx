0b7fbcd13ef7f147dddacbbcac33430a88f115b8
puzann7 <poojanbhatt78@gmail.com>
2025-04-09 17:43:06 +0545

    work on authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebase/firebase";
+import AuthContainer from "../Small components/AuthContainer";
+import AuthInput from "../Small components/AuthInput";
+import { redirect, useNavigate } from "react-router-dom";
 
+const auth = getAuth(app);
 function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword ] = useState("");
+    const loginUser = ()=>{
+        signInWithEmailAndPassword(auth,email,password)
+        .then(value=>navigate("/home"))
+        .catch(err=>alert(err))
+    }
     return (
     <AuthContainer>
-        
     <AuthInput value={email} placeholder="Enter your email" type="email" onChange={(e)=>setEmail(e.target.value)}  />
     <AuthInput value={password} placeholder="Enter your password" type="password" onChange={(e)=>setPassword(e.target.value)}  />
 
-    <button onClick={createUser} className='bg-white cursor-pointer'>Sign Up</button>
+    <button onClick={loginUser} className='bg-white cursor-pointer'>Sign Up</button>
 </AuthContainer>
     )
 
