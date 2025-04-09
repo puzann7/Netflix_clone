242c5a24e58beedca00fa8f79d6fb2b0ac0398cd
puzann7 <poojanbhatt78@gmail.com>
2025-04-09 17:36:15 +0545

    update the dynamic footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { MovieContextProvider } from '../contexts/Movie Category/movieContext'
+import Footer from '../components/Footer'
 function Layout() {
+    const location = useLocation();
+    const hideFooterRoutes = ["/"];
+    const shouldHideFooter = hideFooterRoutes.includes(location.pathname);
   return (
     <MovieContextProvider>
-    <div>
+    <div className='relative '>
       <Outlet />
+      {!shouldHideFooter && <Footer />}
     </div>
     </MovieContextProvider>
   )
