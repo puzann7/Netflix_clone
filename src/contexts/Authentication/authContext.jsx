bfa701ded59028ed5f708a5c2e852178ec9f3119
puzann7 <poojanbhatt78@gmail.com>
2025-04-13 18:49:05 +0545

    get user from firebase and set in context api

--- src/contexts/Authentication/authContext.jsx
+++ src/contexts/Authentication/authContext.jsx
@@ -1,7 +1,8 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { app } from "../../firebase/firebase";
 import { createUserWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { onAuthStateChanged } from "firebase/auth";
 
 
  const authContext = createContext();
@@ -11,6 +12,7 @@ import { getDatabase } from "firebase/database";
  const googleProvider = new GoogleAuthProvider();
 
  export const AuthContextProvider = ({children})=>{
+    const [user,setUser] = useState(null);
     const signUpUserWithEmailAndPassword = (email, password)=>{
         return createUserWithEmailAndPassword(firebaseAuth,email,password)
     }
@@ -20,8 +22,20 @@ import { getDatabase } from "firebase/database";
     const setData = (key,data)=>{
          set(ref(database,key),data);
     }
+    useEffect(() => {
+        onAuthStateChanged(firebaseAuth,(user)=>{
+            if(user){
+                console.log("hello",user);
+                setUser(user);
+            }
+            else{
+                console.log("Logged out");
+                setUser(null);
+            }
+        })
+      })
     return(
-        <authContext.Provider value={{signUpUserWithEmailAndPassword, signUpUserWithGoogle}}>
+        <authContext.Provider value={{signUpUserWithEmailAndPassword, signUpUserWithGoogle, user}}>
             {children}
         </authContext.Provider>
     )
