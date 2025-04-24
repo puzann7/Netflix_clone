426b02969eb34755636f4caf2d221b9b763367e4
puzann7 <poojanbhatt78@gmail.com>
2025-04-24 10:19:08 +0545

    edit the mediaIcon component

--- src/Small components/MediaDisplayIcon.jsx
+++ src/Small components/MediaDisplayIcon.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function MediaDisplayIcon({backdrop,poster}) {
+
+function MediaDisplayIcon({backdrop,poster, ref}) {
     const posterLink = `${import.meta.env.VITE_IMG_URL}w500/${poster}`;
     const backdropLink = `${import.meta.env.VITE_IMG_URL}w500/${backdrop}`;
-
   return (
-    <div className='shrink-0 snap-start  cursor-pointer w-[102px] h-[160px] md:w-[170px] md:h-[272px] lg:w-[218px] lg:h-[123px]  '>
+    <div ref={ref && ref} className='shrink-0 snap-start  cursor-pointer w-[102px] h-[160px] md:w-[170px] md:h-[272px] lg:w-[218px] lg:h-[123px]  '>
         <picture>
         <source media="(min-width: 768px)" srcSet={backdropLink} />
     <img  className=" w-full rounded-sm object-cover   lg:object-center" src={posterLink} alt="error" />
